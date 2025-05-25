10a6b7d73ca40d00521732b2435d1ab3bcaf4b40
abdul muqeet <109543140+armarabdul@users.noreply.github.com>
2025-05-25 09:27:58 +0530

    Update main.py

--- 1. loadingJson_MongoDB/main.py
+++ 1. loadingJson_MongoDB/main.py
@@ -1,11 +1,11 @@
 
 
 # this code will create a collection called transcations and it will import transactions.json which is in array format
-#mongoimport --db usermanaged --collection transactions --file transactions.json --jsonArray
+mongoimport --db usermanaged --collection transactions --file transactions.json --jsonArray
 
 # This code will update and insert which is upsert the document with 
-#mongoimport --db usermanaged --collection transactions --file transactions_upsert.json --jsonArray --mode upsert
+mongoimport --db usermanaged --collection transactions --file transactions_upsert.json --jsonArray --mode upsert
 
 # After running the above commands, check your MongoDB collection: 
-# use usermanaged
-# db.transactions.find().pretty()
+ use usermanaged
+ db.transactions.find().pretty()
